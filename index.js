008f2a93c1ac4eb7a0eeb3d605f2978e670102ec
JStar17 <jstar6819@gmail.com>
2022-02-25 00:08:01 +0530

    user

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express=require("express")
+const mongoose=require("mongoose")
 const sessionController=require("./controller/session-controller")
+const roleController=require("./controller/role-controller")
+const userController = require("./controller/user-controller")
+
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+
+mongoose.connect("mongodb://localhost:27017/HR-Portal",function(err){
+    if(err){
+    console.log(" database connection failed");
+    console.log(err);
+  }else{
+      console.log("database connected");
+  }
+})
 
 app.get("/login",sessionController.login)
 
@@ -12,9 +25,23 @@ app.get("/",function(req,res){
     res.end()
 })
 
+
+//role
+app.post("/roles",roleController.addRole)
+app.get("/roles",roleController.getAllRoles)
+app.delete("/roles/:roleId",roleController.deleteRole)
+app.put("/role",roleController.updateRole)
+
 app.listen (3000,function(req,res){
     console.log("server strated at 3000")
 
 
 })
 
+//user 
+app.post("/users",userController.addUser)
+app.get("/users",userController.getAllUsers)
+app.delete("/users/:userId",userController.deleteUser)
+app.put("/users",userController.updateUser)
+
+app.post("/login",userController.login)
---
+++ model/user_model.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose")
+
+
+const UserSchema = new mongoose.Schema({
+    
+        firstName:{
+            type:String,
+            required:true
+        },
+        lastName:{
+            type:String,
+            required:true
+        },
+        UserName:{
+            type:String,
+            required:true
+        },
+        email:{
+            type:String
+        },
+        password:{
+            type:String
+        },
+        gender:{
+            type:String
+        },
+        mobileNo:{
+            type:Number
+        },
+        education:{
+            type:String
+        },
+        dateOfBirth:{
+            type:Date
+        },
+        address:{
+            type:String
+        },
+        role : {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"role"
+        }
+})
+
+
+const UserModel = mongoose.model("user",UserSchema)
+module.exports = UserModel 
